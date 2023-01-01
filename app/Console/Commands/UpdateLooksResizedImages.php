ab16354267132af5f14dd8a329b4ad3e2fbc4786
Овчинников Егор Вячеславович <eovchinnikov@phoenixit.ru>
2023-01-02 03:35:54 +0400

    testing 2

--- app/Console/Commands/UpdateLooksResizedImages.php
+++ app/Console/Commands/UpdateLooksResizedImages.php
@@ -16,10 +16,12 @@ class UpdateLooksResizedImages extends Command
         $looks = Look::whereNotNull('deleted_at')->cursor();
 
         foreach ($looks as $look) {
+            $this->info("reading image from path: " . '/var/www/storage' . $look->image);
             $image = Image::make('/var/www/storage' . $look->image);
             $image->resize(null, 750, function($constraint) {
                 $constraint->aspectRatio();
             });
+            $this->info("saving resized image to path: " . '/var/www/storage/looks/resized/' . $image->filename . '.' . $image->extension);
             $image->save('/var/www/storage/looks/resized/' . $image->filename . '.' . $image->extension);
 
             $look->update([
