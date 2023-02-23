d271cd57c5ec8d36bbdcc6a06ecdc96607734f4f
sireax <sireax@yandex.ru>
2023-02-24 00:34:37 +0400

    fix articles sex 3

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -12,7 +12,7 @@ class Article extends Model
     protected $table = 'articles';
 
     protected $fillable = [
-        'title', 'slug', 'text', 'cover'
+        'title', 'slug', 'text', 'cover', 'sex'
     ];
 
     public static function sexList()
