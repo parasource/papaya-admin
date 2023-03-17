48d2da72d9ef9bb8c6b02e21c861251bc27309b4
sireax <sireax@yandex.ru>
2023-03-17 22:53:17 +0400

    migrating to new wardrobe database

---
+++ app/Console/Commands/ImportNewWardrobe.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\WardrobeCategory;
+use App\Models\WardrobeItem;
+use App\Models\WardrobeItemDraft;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class ImportNewWardrobe extends Command
+{
+    protected $signature = 'new_wardrobe:import';
+
+    public function handle()
+    {
+        foreach (WardrobeItemDraft::where('status', WardrobeItemDraft::STATUS_APPROVED)->cursor() as $draftItem) {
+            $category = WardrobeCategory::where('name', $draftItem->category)->get();
+            if (!$category) {
+                $category = WardrobeCategory::create([
+                    'name' => $draftItem->category,
+                    'slug' => Str::slug($draftItem->category),
+                    'parent_category' => ' '
+                ]);
+                $this->info("category {$category->name} created");
+            }
+
+            $item = WardrobeItem::create([
+                'name' => $draftItem->name,
+                'slug' => Str::slug($draftItem->name) . "-" . rand(111, 999),
+                'wardrobe_category_id' => $category->id,
+                'image' => $draftItem->image,
+                'sex' => $draftItem->sex,
+                'tags' => ' '
+            ]);
+
+            $this->info("item {$item->name} imported");
+        }
+
+        return Command::SUCCESS;
+    }
+}
