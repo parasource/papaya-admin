92fdbb71eba21a200ca14ec67320c7392bbbaff3
sireax <sireax@yandex.ru>
2023-03-19 00:17:38 +0400

    trying to fix urls

---
+++ app/Console/Commands/FixUrls.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\WardrobeItem;
+use Illuminate\Console\Command;
+
+class FixUrls extends Command
+{
+
+    protected $signature = 'fix:urls';
+
+    public function handle()
+    {
+        foreach (WardrobeItem::where('url', 'like', '%market%')->cursor() as $url) {
+            $url = "https://" . substr($url, 4, strpos($url, "?") - 4);
+            $this->info($url);
+        }
+
+        return Command::SUCCESS;
+    }
+}
