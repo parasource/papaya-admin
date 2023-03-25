9f9457c7a80a5b6d6c69390c67f480ce725ef91e
sireax <sireax@yandex.ru>
2023-03-26 00:37:28 +0400

    fix urls protocol

--- app/Console/Commands/FixUrls.php
+++ app/Console/Commands/FixUrls.php
@@ -12,10 +12,10 @@ class FixUrls extends Command
 
     public function handle()
     {
-        foreach (ItemURL::where('url', 'like', '%redav%')->cursor() as $url) {
-            $leftHalf = substr($url->url, strpos($url, "https://"), strlen($url->url) - strpos($url, "?"));
-            $urlStr = substr($leftHalf, 0, strpos($leftHalf, "&subid"));
-            $this->info($urlStr);
+        foreach (ItemURL::where('url', 'like', 's://%')->cursor() as $url) {
+            $url->update([
+                'url' => 'http' . $url->url
+            ]);
         }
 
         return Command::SUCCESS;
